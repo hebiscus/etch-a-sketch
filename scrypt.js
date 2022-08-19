9342245a971b0945e360390d8874247ba77d96bf
hebi <akikopoland@gmail.com>
2022-08-19 13:10:50 +0200

    added input, css, changes to scrypt

--- scrypt.js
+++ scrypt.js
@@ -5,3 +5,15 @@ for (let i = 0; i < 256; i++) {
     document.getElementById("gridContainer").appendChild(gridElement);
 }
 
+const gridElement = document.getElementsByClassName("gridElement")
+
+document.getElementById("gridContainer").addEventListener("mouseover", function changeColor(event){
+    event.target.style.backgroundColor = 'salmon';
+});
+
+const gridSize = document.getElementById("gridSize");
+const gridValue = document.getElementById("gridSize").value;
+
+// gridSize.addEventListener("input", function(){
+//     document.getElementById("sizeLabel").textContent = `${gridValue}` + " " "x" + " " + `${gridValue}`;
+// });
