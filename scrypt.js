eb459f780cbf381fd41d16dbfe1a1786368a3f46
hebi <akikopoland@gmail.com>
2022-08-19 11:38:50 +0200

    added grid and styling

---
+++ scrypt.js
@@ -0,0 +1,7 @@
+
+for (let i = 0; i < 256; i++) {
+    const gridElement = document.createElement("div");
+    gridElement.classList.add('gridElement');
+    document.getElementById("gridContainer").appendChild(gridElement);
+}
+
