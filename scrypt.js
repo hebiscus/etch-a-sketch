e2ebe47ed5ad34da847551e5264f89449be27aff
hebi <akikopoland@gmail.com>
2022-08-20 19:21:51 +0200

    added remove grid and attempt and making a new one

--- scrypt.js
+++ scrypt.js
@@ -22,3 +22,14 @@ gridSize.addEventListener("input", function () {
     gridValue.textContent = gridSize.value + " " + "x " + gridSize.value;
 });
 
+gridSize.addEventListener("input", function () {
+    const gridContainer = document.getElementById("gridContainer");
+    while (gridContainer.firstChild) {
+        gridContainer.firstChild.remove()
+    }
+    for (let i = 0; i < gridSize.value; i++) {
+        const gridElement = document.createElement("div");
+        gridElement.classList.add('gridElement');
+        document.getElementById("gridContainer").appendChild(gridElement);
+    }
+});
